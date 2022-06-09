bde821596fcfd5f2836906fd2b06e3b76946e099
Dongchul Choi <donchoi@c5r1s1.42seoul.kr>
2022-06-09 15:00:37 +0900

    remove stdio.h

--- minitalk.h
+++ minitalk.h
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: donchoi <donchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: donchoi <donchoi.student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 14:42:57 by donchoi           #+#    #+#             */
-/*   Updated: 2022/06/03 14:42:57 by donchoi          ###   ########.fr       */
+/*   Updated: 2022/06/09 14:58:41 by donchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <stdio.h>
 # include <signal.h>
 # include "libft/libft.h"
 
